50e74d6fdd8a7988f205a5db3518356dae1c06b3
l-hepper <liamhepper1@gmail.com>
2024-02-25 16:03:47 +0000

    add basic navbar

---
+++ src/components/navbar.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import "./navbar.css";
+
+export default function Navbar() {
+    return (
+        <header>
+            <div class="samurise-brand">
+                <h1>Samurise</h1>
+            </div>
+            <nav class="navbar">
+                <ul class="navbar-list">
+                    <li class="navbar-item">
+                        <a href="dashboard">Dashboard</a>
+                    </li>
+                    <li class="navbar-item">
+                        <a href="stats">Stats</a>
+                    </li>
+                    <li class="navbar-item">
+                        <a href="settings">Settings</a>
+                    </li>
+                </ul>
+            </nav>
+        </header>
+    );
+}
