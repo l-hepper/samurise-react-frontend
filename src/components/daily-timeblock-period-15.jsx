9852eb817806247d8e6e9246f896da753e093dc1
l-hepper <liamhepper1@gmail.com>
2024-02-26 16:32:27 +0000

    adds rudimentary timeblock selection through a boolean newTimeBlockMode

--- src/components/daily-timeblock-period-15.jsx
+++ src/components/daily-timeblock-period-15.jsx
@@ -1,22 +1,28 @@
 import React from "react";
-import {useState} from "react";
+import { useState } from "react";
 
-export default function DailyTimeBlockPeriod15() {
-    let nonSelectedStyling = "daily-timeblock-period-15";
-    let selectedStyling = "daily-timeblock-period-15 selected";
+export default function DailyTimeBlockPeriod15(props) {
 
-    const [isSelected, setSelected] = useState(false);
+  let nonSelectedStyling = "daily-timeblock-period-15";
+  let selectedStyling = "daily-timeblock-period-15 selected";
 
-    function onClick() {
-        if (isSelected) {
-            setSelected(false);
-        } else {
-            setSelected(true);
-        }
+  const [isSelected, setSelected] = useState(false);
+
+  function onClick() {
+    if (isSelected) {
+      setSelected(false);
+    } else {
+      setSelected(true);
     }
+    props.onClick(props.timePeriod);
+  }
+
   return (
-    <div onClick={onClick} class={isSelected ? selectedStyling : nonSelectedStyling}>
-      <p>Test</p>
+    <div
+      onClick={onClick}
+      class={isSelected ? selectedStyling : nonSelectedStyling}
+    >
+      <p>{props.timePeriod}</p>
     </div>
   );
 }
--- src/components/daily-timeblock.jsx
+++ src/components/daily-timeblock.jsx
@@ -1,25 +1,47 @@
 import React from "react";
+import { useState } from "react";
 
 import "./daily-timeblock.css";
 
 import OneHourTimeBlock from "./one-hour-time-block";
 
 export default function DailyTimeBlock() {
+  let timeBlockRange = [8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19];
+
+  const [startTime, updateStartTime] = useState();
+  const [endTime, updateEndTime] = useState();
+  const [newTimeBlockMode, setTimeBlockMode] = useState(false);
+
+  // example timeblock object for debugging and testing
+  const timeBlockExample = {
+    startTime: startTime,
+    endTime: endTime,
+  };
+
+  const handleTimeBlockClick = (timeSelected) => {
+    if (!newTimeBlockMode) {
+      timeBlockExample.startTime = updateStartTime(timeSelected);
+      setTimeBlockMode(true);
+    }
+
+    if (newTimeBlockMode) {
+      timeBlockExample.endTime = updateEndTime(timeSelected);
+      setTimeBlockMode(false);
+    }
+  };
+
   return (
     <div class="daily-timeblock">
-      <h6>8am - 8pm</h6>
-      <OneHourTimeBlock backColour="white"/>
-      <OneHourTimeBlock backColour="gray"/>
-      <OneHourTimeBlock backColour="white"/>
-      <OneHourTimeBlock backColour="gray"/>
-      <OneHourTimeBlock backColour="white"/>
-      <OneHourTimeBlock backColour="gray"/>
-      <OneHourTimeBlock backColour="white"/>
-      <OneHourTimeBlock backColour="gray"/>
-      <OneHourTimeBlock backColour="white"/>
-      <OneHourTimeBlock backColour="gray"/>
-      <OneHourTimeBlock backColour="white"/>
-      <OneHourTimeBlock backColour="gray"/>
+      <h6>
+        {timeBlockExample.startTime} - {timeBlockExample.endTime}
+      </h6>
+      {timeBlockRange.map((item) => (
+        <OneHourTimeBlock
+          backColour="white"
+          startHour={item}
+          onClick={handleTimeBlockClick}
+        />
+      ))}
     </div>
   );
 }
