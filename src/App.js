cf2cbd6ce6a5b00806b9b0a9fd4592c0bc801cf2
l-hepper <liamhepper1@gmail.com>
2024-03-04 12:58:22 +0000

    sections added for habit-tracker, pomodoro, tasklist sections

--- src/App.js
+++ src/App.js
@@ -3,15 +3,31 @@ import React, { Component } from "react";
 import Navbar from "./components/navbar";
 import DailyTimeBlock from "./components/daily-timeblock";
 import DateSelect from "./components/date-select";
+import Pomodoro from "./components/pomodoro";
+
+function handleTimeChange() {
+  alert("Time change selected");
+}
 
 class App extends Component {
   render() {
     return (
       <div>
         <Navbar />
-        <div class="left-page">
-          <DateSelect />
-          <DailyTimeBlock />
+        <DateSelect />
+        <div class="samurise-dashboard">
+          <div class="left-page">
+            <DailyTimeBlock />
+          </div>
+          <div class="right-page">
+            <div class="habit-tracker">
+              <p>Habit Tracker</p>
+            </div>
+            <Pomodoro />
+            <div class="todo">
+              <p>Todo List</p>
+            </div>
+          </div>
         </div>
       </div>
     );
