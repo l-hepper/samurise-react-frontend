854694d87bfbde22bf1cc4ea91f0e40076510502
l-hepper <liamhepper1@gmail.com>
2024-03-05 13:46:56 +0000

    fix - add explicit height to samurise dashboard container to ensure it does not resize dependant on length of timeblock planner

--- src/components/pomodoro.jsx
+++ src/components/pomodoro.jsx
@@ -32,7 +32,7 @@ export default function Pomodoro() {
     if (countingDown) {
       intervalId = setInterval(() => {
         setSecondsLeft((prevSeconds) => prevSeconds - 1);
-      }, 1000);
+      }, 25);
     }
 
     return () => clearInterval(intervalId);
